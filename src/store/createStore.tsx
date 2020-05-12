24b3cd00f16c8953bee25ae3decb5790ad9404d9
Hector Quintero <isc.quch@gmail.com>
2020-05-12 16:04:09 -0500

    feat: add mock data
    TechM Assignment

--- src/store/createStore.tsx
+++ src/store/createStore.tsx
@@ -1,7 +1,10 @@
 import { Student } from '../models/form.model';
+import { mock } from './mockData';
+
+const studentList: Student[] = mock;
 
 export const createStore = () => ({
-  studentList: [] as Student[],
+  studentList,
   addStudent(student: Student) {
     this.studentList.push(student);
   },
@@ -9,11 +12,9 @@ export const createStore = () => ({
     const newList = this.studentList.filter((currentStudent) => currentStudent.id !== student.id);
     this.studentList = [...newList];
   },
-  // getStudent(id: number): Student {
-  //   const student = this.studentList.find((student: Student) => student.id === id);
-
-  //   return student;
-  // }
+  getStudent(id: string) {
+    return this.studentList.find((student: Student) => student.id === Number(id));
+  },
 });
 
 export type StudentStore = ReturnType<typeof createStore>;
---
+++ src/store/mockData.js
@@ -0,0 +1,90 @@
+export const mock = [
+  {
+    address: '123 red street',
+    city: 'irving',
+    firstName: 'Otis',
+    gpa: '3.7',
+    id: 1589315711264,
+    lastName: 'Milburn',
+    phone: '2141234567',
+    photo:
+      'https://vignette.wikia.nocookie.net/sex-education-netflix/images/4/43/Otis_Milburn_Season_1_Portrait.jpg/revision/latest?cb=20190902201801',
+  },
+  {
+    address: '456 orange street',
+    city: 'irving',
+    firstName: 'Maeve',
+    gpa: '4',
+    id: 1589316043366,
+    lastName: 'Wiley',
+    phone: '2147891632',
+    photo:
+      'https://vignette.wikia.nocookie.net/sex-education-netflix/images/a/a8/Maeve_Wiley_Season_1_Portrait.jpg/revision/latest?cb=20190902212135',
+  },
+  {
+    address: '789 pink street',
+    city: 'irving',
+    firstName: 'Eric',
+    gpa: '3.5',
+    id: 1589316081544,
+    lastName: 'Effiong',
+    phone: '2149876543',
+    photo:
+      'https://vignette.wikia.nocookie.net/sex-education-netflix/images/9/9c/Eric_Effiong_Season_1_Portrait.jpg/revision/latest?cb=20190902205935',
+  },
+  {
+    address: '654 brown street',
+    city: 'irving',
+    firstName: 'Adam',
+    gpa: '1.2',
+    id: 1589316153695,
+    lastName: 'Groff',
+    phone: '2145987631',
+    photo:
+      'https://vignette.wikia.nocookie.net/sex-education-netflix/images/3/3c/Adam_Groff_Season_1_Portrait.jpg/revision/latest?cb=20190902213051',
+  },
+  {
+    address: '951 purple street',
+    city: 'irving',
+    firstName: 'Aimee',
+    gpa: '2.8',
+    id: 1589316204002,
+    lastName: 'Gibbs',
+    phone: '2143571596',
+    photo:
+      'https://vignette.wikia.nocookie.net/sex-education-netflix/images/5/57/Aimee_Gibbs_Season_1_Portrait.jpg/revision/latest?cb=20190902221209',
+  },
+  {
+    address: '564 grey street',
+    city: 'irving',
+    firstName: 'Jackson',
+    gpa: '2.2',
+    id: 1589316266923,
+    lastName: 'Marchetti',
+    phone: '2148521476',
+    photo:
+      'https://vignette.wikia.nocookie.net/sex-education-netflix/images/5/59/Jackson_Marchetti_Season_1_Portrait.jpg/revision/latest?cb=20190902213612',
+  },
+  {
+    address: '984 magenta street',
+    city: 'irving',
+    firstName: 'Lily',
+    gpa: '3.2',
+    id: 1589316348945,
+    lastName: 'Iglehart',
+    phone: '2147456328',
+    photo:
+      'https://vignette.wikia.nocookie.net/sex-education-netflix/images/9/93/Lily_Iglehart_Season_1_Portrait.jpg/revision/latest?cb=20190903013027',
+  },
+  {
+    address: '963 cyan street',
+    city: 'irving',
+    firstName: 'Ola',
+    gpa: '3.7',
+    id: 1589316403709,
+    lastName: 'Nyman',
+    phone: '2143257413',
+    photo:
+      'https://vignette.wikia.nocookie.net/sex-education-netflix/images/3/34/Ola_Nyman_Season_1_Portrait.jpg/revision/latest?cb=20190903014055',
+  },
+];
