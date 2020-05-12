51d4439020f2682a67b28aaad22f1ebd045bcd47
Hector Quintero <isc.quch@gmail.com>
2020-05-12 16:18:43 -0500

    styles: update Header styles
    TechM Assignment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 export const Header: React.FC = () => (
-  <nav>
-    <NavLink to="/" exact>
-      Students
-    </NavLink>
-    {'|'}
-    <NavLink to="/student">Add Student</NavLink>
+  <nav className="navbar" role="navigation" aria-label="main navigation">
+    <div className="navbar-menu">
+      <NavLink className="navbar-item is-tab" activeClassName="is-active" to="/" exact>
+        Students
+      </NavLink>
+      <NavLink className="navbar-item is-tab" activeClassName="is-active" to="/student">
+        Add Student
+      </NavLink>
+    </div>
   </nav>
 );
