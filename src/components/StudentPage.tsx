b12d0df897494877c9eb3cc6440d8f788ad66ea0
Hector Quintero <isc.quch@gmail.com>
2020-05-13 08:24:12 -0500

    feat: redirect when no student is found
    TechM Assignment

--- src/components/StudentPage.tsx
+++ src/components/StudentPage.tsx
@@ -1,64 +1,69 @@
 import React from 'react';
-import { RouteComponentProps } from 'react-router-dom';
+import { RouteComponentProps, Redirect } from 'react-router-dom';
 import { useStore } from '../store/store';
+import dummy from '../dummy-prod-1.jpg';
 
 interface StudentPageProps extends RouteComponentProps<{ id: string }> {}
 
-export const StudentPage: React.FC<StudentPageProps> = ({ history, match }) => {
+export const StudentPage: React.FC<StudentPageProps> = ({ match }) => {
   const { id } = match.params;
   const store = useStore();
   const student = store.getStudent(id);
   const fullName = `${student?.firstName} ${student?.lastName}`;
 
   return (
-    <nav className="box">
-      <div className="columns">
-        <div className="column is-one-quarter">
-          <img src={student?.photo} alt={fullName} width="200" />
+    <>
+      {!student && <Redirect to="/" />}
+      <h2 className="title">Student Details</h2>
+      <nav className="box">
+        <div className="columns">
+          <div className="column is-one-quarter">
+            <img src={student?.photo ? student.photo : dummy} alt={fullName} width="200" />
+          </div>
+          <div className="column">
+            <table className="table is-fullwidth">
+              <tbody>
+                <tr>
+                  <td>
+                    <strong>First Name:</strong>
+                  </td>
+                  <td>{student?.firstName}</td>
+                </tr>
+                <tr>
+                  <td>
+                    <strong>Last Name:</strong>
+                  </td>
+                  <td>{student?.lastName}</td>
+                </tr>
+                <tr>
+                  <td>
+                    <strong>Address:</strong>
+                  </td>
+                  <td>{student?.address}</td>
+                </tr>
+                <tr>
+                  <td>
+                    <strong>City:</strong>
+                  </td>
+                  <td>{student?.city}</td>
+                </tr>
+                <tr>
+                  <td>
+                    <strong>Phone:</strong>
+                  </td>
+                  <td>{student?.phone}</td>
+                </tr>
+                <tr>
+                  <td>
+                    <strong>GPA:</strong>
+                  </td>
+                  <td>{student?.gpa}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-        <div className="column">
-          <table className="table is-fullwidth">
-            <tbody>
-              <tr>
-                <td>
-                  <strong>First Name:</strong>
-                </td>
-                <td>{student?.firstName}</td>
-              </tr>
-              <tr>
-                <td>
-                  <strong>Last Name:</strong>
-                </td>
-                <td>{student?.lastName}</td>
-              </tr>
-              <tr>
-                <td>
-                  <strong>Address:</strong>
-                </td>
-                <td>{student?.address}</td>
-              </tr>
-              <tr>
-                <td>
-                  <strong>City:</strong>
-                </td>
-                <td>{student?.city}</td>
-              </tr>
-              <tr>
-                <td>
-                  <strong>Phone:</strong>
-                </td>
-                <td>{student?.phone}</td>
-              </tr>
-              <tr>
-                <td>
-                  <strong>GPA:</strong>
-                </td>
-                <td>{student?.gpa}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
